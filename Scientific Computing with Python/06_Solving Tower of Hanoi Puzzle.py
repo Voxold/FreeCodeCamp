425d1a7e8cdf4b8d175cce666e665524a7565c2b
Voxold <bilalajaha@gmail.com>
2024-02-22 23:28:59 +0100

    Puzzl

--- Scientific Computing with Python/06_Solving Tower of Hanoi Puzzle.py
+++ Scientific Computing with Python/06_Solving Tower of Hanoi Puzzle.py
@@ -6,10 +6,10 @@ C = []
 def move(n, source, auxiliary, target):
     if n <= 0:
         return
-        
+
     #move n - 1 disks from source to auxiliary, so they are out of the way
     move(n - 1, source, target, auxiliary)
-
+    
     #move the nth disk from source to target
     target.append(source.pop())
 
